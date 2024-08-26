7cc273e76743a7c35e695dd2d4fa631bd2e1fd5f
arshan2 <arshan@peppo.co.in>
2024-08-26 14:08:14 +0530

    build dark/light mode switch

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,12 @@
 import { HStack, Image } from "@chakra-ui/react"
 import logo from "../assets/game-control.webp"
+import ColorModeSwitch from "./ColorModeSwitch"
 
 const Navbar = () => {
   return (
-    <HStack padding="20px">
-        <Image src={logo} boxSize="70px" />
+    <HStack padding="20px" justifyContent="space-between">
+        <Image src={logo} boxSize="60px" />
+        <ColorModeSwitch />
     </HStack>
   )
 }
